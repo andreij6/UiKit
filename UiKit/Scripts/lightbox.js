2d980538d05f1dd2a64b5c9a4d8e9d53438ca679
andreij6 <andreij6@gmail.com>
2014-07-16 01:47:04 -0500

    added Todo page css and jQuery

---
+++ UiKit/Scripts/lightbox.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+var $overlay = $('<div class="overlay container"></div>');
+var $textarea = $('<textarea class="form-control" rows="3" placeholder="Add something to be done" id="styled"></textarea>');
+
+$overlay.append($textarea);
+
+$(".container").append($overlay);
+
+var toggleStatus = function () {
+
+    if($(this).hasClass('checked'))
+    {
+        $(this).removeClass('checked');
+        $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-unchecked');
+    } else {
+        $(this).addClass('checked');
+        $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+
+    }
+}
+
+var showLightBox = function () {
+    //Show the overlay
+    $overlay.show();
+    //Update the overlay with the text-box, save button and map 
+}
+
+var hideOverlay = function () {
+    //$overlay.hide();
+}
+
+//Click the button
+$("#newTodo").on('click', showLightBox);
+
+$(".overlay").on('click', hideOverlay);
+
+$('.timeline-badge').on('click', toggleStatus);
